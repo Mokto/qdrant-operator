b9332176277748b5e2aafdaa45e190e9a7dce4b4
Theo Mathieu <tmathieu.github@fastmail.com>
2024-09-27 08:44:20 +0200

    fix: restart failed ephemeral if consensus if failing for > 15s

--- internal/statushandler/thread_status_watch.go
+++ internal/statushandler/thread_status_watch.go
@@ -23,7 +23,7 @@ func (s *StatusHandler) checkForConsensusThreadStatus() {
 	}
 
 	for {
-		time.Sleep(10 * time.Second)
+		time.Sleep(30 * time.Second)
 
 		clusters := &qdrantv1alpha1.QdrantClusterList{}
 		err := s.manager.GetClient().List(s.ctx, clusters)
@@ -34,7 +34,10 @@ func (s *StatusHandler) checkForConsensusThreadStatus() {
 		for _, cluster := range clusters.Items {
 
 			for _, peer := range cluster.Status.Peers {
-				threadStatus, err := s.getConsensusThreadStatus(peer.DNS)
+				if !peer.EphemeralStorage {
+					continue
+				}
+				threadStatus, raftTerm, raftCommit, err := s.getConsensusThreadStatus(peer.DNS)
 				if err != nil {
 					s.log.Error(err, "unable to get consensus thread status")
 					continue
@@ -47,26 +50,43 @@ func (s *StatusHandler) checkForConsensusThreadStatus() {
 					}
 					s.log.Info("Consensus thread stopped. Restarting the pod " + peer.PodName + ".")
 				}
+				if raftTerm == 0 && raftCommit == 0 {
+					s.log.Info("raftTerm and raftCommit are 0 for peer " + peer.DNS + ". Waiting 30s and killing the pod if that still the case")
+					time.Sleep(15 * time.Second)
+					_, raftTerm, raftCommit, err = s.getConsensusThreadStatus(peer.DNS)
+					if err != nil {
+						s.log.Error(err, "unable to get consensus thread status")
+						continue
+					}
+					if raftTerm == 0 && raftCommit == 0 {
+						err := clientset.CoreV1().Pods(cluster.Namespace).Delete(s.ctx, peer.PodName, v1.DeleteOptions{})
+						if err != nil {
+							s.log.Error(err, "unable to delete pod")
+							continue
+						}
+						s.log.Info("raftTerm and raftCommit are 0 for peer " + peer.DNS + ". Restarting the pod " + peer.PodName + ".")
+					}
+				}
 			}
 		}
 
 	}
 }
 
-func (s *StatusHandler) getConsensusThreadStatus(serviceName string) (string, error) {
+func (s *StatusHandler) getConsensusThreadStatus(serviceName string) (status string, raftTerm uint64, raftCommit uint64, err error) {
 
 	resp, err := http.Get("http://" + serviceName + ":6333/cluster")
 	if err != nil {
 		s.log.Error(err, "unable to get cluster info")
-		return "", err
+		return "", 0, 0, err
 	}
 	body, err := io.ReadAll(resp.Body)
 	if err != nil {
 		s.log.Error(err, "unable to read response body")
-		return "", err
+		return "", 0, 0, err
 	}
 	resp.Body.Close()
 	bodyString := string(body)
 
-	return gjson.Get(bodyString, "result.consensus_thread_status.consensus_thread_status").String(), nil
+	return gjson.Get(bodyString, "result.consensus_thread_status.consensus_thread_status").String(), gjson.Get(bodyString, "result.raft_info.term").Uint(), gjson.Get(bodyString, "result.raft_info.commit").Uint(), nil
 }
